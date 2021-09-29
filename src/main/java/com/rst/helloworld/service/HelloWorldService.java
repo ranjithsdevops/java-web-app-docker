c32afacbf8538deb93c9bbe5cf869603e29ad0f7
ranjithsdevops <90316395+ranjithsdevops@users.noreply.github.com>
2021-09-29 20:23:21 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Dockerr";
+			return "Hello Dockerrr";
 		}else{
 			return "Hello " + name;
 		}
